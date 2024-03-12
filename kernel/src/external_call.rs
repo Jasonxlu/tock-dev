a964cf9ac4c9415de42a63523d1a6935d6e1beb2
Jason <jasonlu2025@u.northwestern.edu>
2024-03-11 19:21:14 -0500

    Cleaned up the external call file

--- kernel/src/external_call.rs
+++ kernel/src/external_call.rs
@@ -1,44 +1,42 @@
-use crate::platform::chip::Chip;
-use crate::platform::platform::KernelResources;
-use crate::process::{self, Process, ProcessId, ShortID, Task};
-use crate::syscall::Syscall;
+// Licensed under the Apache License, Version 2.0 or the MIT License.
+// SPDX-License-Identifier: Apache-2.0 OR MIT
+// Copyright Tock Contributors 2022.
 
-use crate::syscall::{ContextSwitchReason, SyscallReturn};
+//! This module contains the implementation of the `ExternalCall` struct, which
+//! is used to handle external syscalls to other boards from the application side.
 
 use crate::debug;
-
 use crate::errorcode::ErrorCode;
-
-use crate::platform::platform::{ProcessFault, SyscallDriverLookup, SyscallFilter};
-
-use crate::syscall::SyscallDriver;
-
-use crate::syscall_driver::CommandReturn;
-
-use crate::hil::uart; // import uart
-use crate::utilities::cells::{MapCell, TakeCell};
-
-// import the kernel
+use crate::hil::uart;
 use crate::kernel::Kernel;
+use crate::platform::chip::Chip;
+use crate::platform::platform::KernelResources;
+use crate::platform::platform::SyscallDriverLookup;
+use crate::process::{Process, ProcessId};
+use crate::syscall::Syscall;
+use crate::syscall::SyscallReturn;
+use crate::syscall_driver::CommandReturn;
+use crate::utilities::cells::TakeCell;
 
 /// This bool tracks whether there are any external calls pending for service.
 static mut JOB_PENDING: bool = false;
+/// This bool tracks whether a response has been received from the other board
+/// and if we can send the next syscall
 static mut SEND_TRANSMIT: bool = true;
 
+/// ExternalCall struct
 pub struct ExternalCall {
-    kernel: &'static Kernel,
+    _kernel: &'static Kernel,
     processid: ProcessId,
-
-    //TODO:: buffer
     uart: &'static dyn uart::UartData<'static>,
     tx_buffer: TakeCell<'static, [u8]>,
     rx_buffer: TakeCell<'static, [u8]>,
-
     curr_syscall: TakeCell<'static, [u8]>,
 }
 
+/// Implement the ExternalCall struct
 impl ExternalCall {
-    /// Creates a new deferred call with a unique ID.
+    /// Create a new ExternalCall object.
     pub fn new(
         kernel: &'static Kernel,
         uart: &'static dyn uart::UartData,
@@ -46,16 +44,14 @@ impl ExternalCall {
         rx_buffer: &'static mut [u8],
         curr_syscall: &'static mut [u8],
     ) -> Self {
-        // SAFETY: No accesses to CTR are via an &mut, and the Tock kernel is
-        // single-threaded so all accesses will occur from this thread.
-
+        // Create a unique identifier for the process
         let unique_identifier = kernel.create_process_identifier();
 
-        // Create a dummy processid //TODO: Unsure about what to put for index
+        // Create a dummy processid
         let processid = ProcessId::new(kernel, unique_identifier, 0);
 
         ExternalCall {
-            kernel: kernel,
+            _kernel: kernel,
             processid: processid,
             uart: uart,
             tx_buffer: TakeCell::new(tx_buffer),
@@ -64,66 +60,50 @@ impl ExternalCall {
         }
     }
 
-    // ExternalCall.start_transmission()
+    /// Start the transmission of the buffer.
     pub fn start_transmission(&self, buffer: &[u8]) -> Result<(), ErrorCode> {
-        debug!("Started transmission");
         self.tx_buffer
             .take()
             .map_or(Err(ErrorCode::BUSY), |tx_buf| {
                 for (i, c) in buffer.iter().enumerate() {
                     if i < tx_buf.len() {
                         tx_buf[i] = *c;
-                    } else {
-                        debug!("buffer too big");
                     }
                 }
-                // let copy_len = dest.len().max(len);
 
-                // dest[0..copy_len].copy_from_slice(&buffer[0..copy_len]);
-                // }
                 let len = tx_buf.len();
                 let result = self.uart.transmit_buffer(tx_buf, len);
                 match result {
                     Ok(()) => Ok(()),
-                    Err((code, buffer)) => {
-                        // self.tx_buffer.replace(buffer);
-                        Err(code)
-                    }
+                    Err((code, _buffer)) => Err(code),
                 }
             })
     }
 
-    // ExternalCall.receive(&self)
+    /// Start the reception of the buffer.
     pub fn receive(&self) -> Result<(), ErrorCode> {
-        debug!("Started reception");
-
         self.rx_buffer
             .take()
             .map_or(Err(ErrorCode::ALREADY), |rx_buf| {
                 let len = rx_buf.len();
                 let result: Result<(), (ErrorCode, &mut [u8])> =
                     self.uart.receive_buffer(rx_buf, len);
-                debug!("inside receive");
+
                 match result {
                     Ok(()) => Ok(()),
-                    Err((code, buffer)) => {
-                        debug!("something went wrong");
-                        // self.rx_buffer.replace(buffer);
-                        Err(code)
-                    }
+                    Err((code, _buffer)) => Err(code),
                 }
             })
     }
 
-    /// Schedule a deferred callback on the client associated with this deferred call
+    /// Set the 'JOB_PENDING' flag to true.
     pub fn set(&self) {
-        // SAFETY: No accesses to BITMASK are via an &mut, and the Tock kernel is
-        // single-threaded so all accesses will occur from this thread.
         unsafe {
             JOB_PENDING = true;
         }
     }
 
+    /// Checks if the driver number is external or not.
     pub fn driver_num_is_external(&self, driver_num: usize) -> bool {
         if driver_num >> 31 == 1 {
             return true;
@@ -132,15 +112,15 @@ impl ExternalCall {
         }
     }
 
-    /// Returns true if any deferred calls are waiting to be serviced,
+    /// Returns true if an external call is waiting to be serviced,
     /// false otherwise.
     pub fn has_tasks() -> bool {
-        // SAFETY: No accesses to BITMASK are via an &mut, and the Tock kernel is
-        // single-threaded so all accesses will occur from this thread.
         unsafe { JOB_PENDING }
     }
 
-    // Return an array of u8 that represents the syscall
+    /// Function to pack the syscall and send it
+    /// Only sends the syscall if the SEND_TRANSMIT flag is set to true
+    /// (When we receive a response from the other board)
     pub fn pack_syscall_and_send(&self, syscall: Syscall) {
         if let Syscall::Command {
             driver_number,
@@ -171,7 +151,7 @@ impl ExternalCall {
             let send_transmit = unsafe { SEND_TRANSMIT };
 
             if send_transmit {
-                self.start_transmission(&buffer);
+                let _ = self.start_transmission(&buffer);
             }
 
             unsafe {
@@ -180,22 +160,21 @@ impl ExternalCall {
         }
     }
 
+    /// Function to unpack the bytes received from the other board
+    /// into a Syscall
     pub fn unpack_bytes(&self) -> Result<Syscall, ErrorCode> {
-        debug!("started unpacking");
         self.curr_syscall.map_or(Err(ErrorCode::INVAL), |rx_buf| {
             let mut driver_number: usize = 0;
             for i in 1..5 {
                 driver_number = driver_number << 8;
                 driver_number = driver_number | rx_buf[i] as *const u8 as usize;
             }
-            debug!("This is the driver_number {}", driver_number);
 
             let mut subdriver_number: usize = 0;
             for i in 5..9 {
                 subdriver_number = subdriver_number << 8;
                 subdriver_number = subdriver_number | rx_buf[i] as *const u8 as usize;
             }
-            debug!("This is the subdriver number {}", subdriver_number);
 
             let mut arg0: usize = 0;
             for i in 9..13 {
@@ -203,14 +182,11 @@ impl ExternalCall {
                 arg0 = arg0 | rx_buf[i] as *const u8 as usize;
             }
 
-            debug!("This is the arg0 {}", arg0);
-
             let mut arg1: usize = 0;
             for i in 13..17 {
                 arg1 = arg1 << 8;
                 arg1 = arg1 | rx_buf[i] as *const u8 as usize;
             }
-            debug!("This is arg1 {}", arg1);
 
             Ok(Syscall::Command {
                 driver_number,
@@ -221,32 +197,28 @@ impl ExternalCall {
         })
     }
 
-    /// Services and clears the next pending `DeferredCall`, returns which index
-    /// was serviced
+    /// Services and clears the pending External Syscall, if any.
     pub fn service_next_pending<KR: KernelResources<C>, C: Chip>(&self, resources: &KR) {
-        // SAFETY: No accesses to BITMASK/DEFCALLS are via an &mut, and the Tock kernel is
-        // single-threaded so all accesses will occur from this thread.
         let job = unsafe { JOB_PENDING };
         if job {
             unsafe {
                 JOB_PENDING = false;
             }
 
-            let syscall = self.unpack_bytes().unwrap(); // Unwrap the Result twice to get the Syscall value
+            let syscall = self.unpack_bytes().unwrap();
 
             self.handle_external_syscall::<_, _>(resources, self.processid, syscall);
         }
     }
-    // Function to handle external syscalls and process them
+
+    /// Function to handle external syscalls from the other board
+    /// and call the appropriate driver
     pub fn handle_external_syscall<KR: KernelResources<C>, C: Chip>(
         &self,
         resources: &KR,
-        // process: &dyn process::Process,
         processid: ProcessId,
         syscall: Syscall,
     ) {
-        // Hook for process debugging.
-
         // Handles only the `Command` syscall
         if let Syscall::Command {
             driver_number,
@@ -263,31 +235,27 @@ impl ExternalCall {
                         None => CommandReturn::failure(ErrorCode::NODEVICE),
                     };
 
-                    let res = SyscallReturn::from_command_return(cres);
+                    let _res = SyscallReturn::from_command_return(cres);
 
                     let mut return_buffer: [u8; 17] = [0; 17];
                     return_buffer[0] = 2;
 
-                    debug!("Sent a response");
-
-                    self.start_transmission(&return_buffer); // TODO: << Figure out what to do about process here
+                    let _ = self.start_transmission(&return_buffer);
                 });
         }
     }
 }
 
+/// Implement the TransmitClient and ReceiveClient for ExternalCall
 impl uart::TransmitClient for ExternalCall {
     fn transmitted_buffer(
         &self,
         buffer: &'static mut [u8],
-        tx_len: usize,
-        rval: Result<(), ErrorCode>,
+        _tx_len: usize,
+        _rval: Result<(), ErrorCode>,
     ) {
-        // debug!("Completed transmission");
         self.tx_buffer.replace(buffer);
-
-        // debug!("Calling reception from tx callback");
-        let result = self.receive();
+        let _result = self.receive();
     }
     fn transmitted_word(&self, _rval: Result<(), ErrorCode>) {}
 }
@@ -296,42 +264,20 @@ impl uart::ReceiveClient for ExternalCall {
     fn received_buffer(
         &self,
         buffer: &'static mut [u8],
-        rx_len: usize,
-        rcode: Result<(), ErrorCode>,
-        error: uart::Error,
+        _rx_len: usize,
+        _rcode: Result<(), ErrorCode>,
+        _error: uart::Error,
     ) {
-        debug!("Completed reception");
         let id = buffer[0];
-        debug!("{}", id);
 
         self.curr_syscall.map(|curr_sys| {
             for (i, c) in buffer.iter().enumerate() {
                 if i < curr_sys.len() {
                     curr_sys[i] = *c;
-                } else {
-                    debug!("buffer too big");
                 }
             }
         });
-
-        // debug!("Reception completed");
-        //     }
-        //     Ok(()),
-        //     Err(code) => {
-        //         debug!("{:?}", code);
-        //     }
     }
-
-    // let transmission_result: Result<(), ErrorCode> = self.start_transmission(&new_buffer);
-    // if let Err(code) = transmission_result {
-    //     debug!("{:?}", code);
-    // } else {
-    //     debug!("transmit complete");
-    // }
-    // check result/error code
-
-    //return result
-    //clean up
 
     fn received_word(&self, _word: u32, _rval: Result<(), ErrorCode>, _error: uart::Error) {}
 }
